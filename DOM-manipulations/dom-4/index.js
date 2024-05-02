d4cc4ebd06582dcd3bb85316825f078ef127d99d
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-05-03 00:33:01 +0530

    added till dom no 5

---
+++ DOM-manipulations/dom-4/index.js
@@ -0,0 +1,5 @@
+// Write your code below:
+const temp=document.getElementsByClassName('fruit');
+temp[2].style.backgroundColor="yellow";
+for(let i=0;i<4;i++)
+temp[i].style.fontWeight="bold";
---
+++ DOM-manipulations/dom-5/index.js
@@ -0,0 +1,6 @@
+// Write your code below:
+const temp=document.getElementsByTagName('li');
+temp[4].style.color="blue";
+
+for(let i=0;i<temp.length;i++)
+  temp[i].style.fontStyle="italic"
