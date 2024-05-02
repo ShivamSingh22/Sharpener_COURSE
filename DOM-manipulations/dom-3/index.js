69943032514ba16d733c171ca4633ef5d10000a2
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-05-03 00:29:40 +0530

    initial commit

---
+++ DOM-manipulations/dom-3/index.js
@@ -0,0 +1,13 @@
+// Write your code below:
+let header=document.getElementById('header');
+let main1=document.getElementById('main-heading');
+main1.style.color='orange';
+header.style.backgroundColor='green';
+header.style.borderBottom='2px solid orange';
+main1.innerHTML="Fruit World";
+
+let basket=document.getElementById('basket-heading');
+basket.style.color='green';
+
+let txt=document.getElementById('thanks');
+txt.innerHTML="<p>Please visit us again</p>"
