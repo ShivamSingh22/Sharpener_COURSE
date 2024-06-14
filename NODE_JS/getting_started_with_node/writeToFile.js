57f75a93d711f20b17910684cf069dbcba81bc63
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-06-14 23:37:24 +0530

    read and write from a file

---
+++ NODE_JS/getting_started_with_node/writeToFile.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+
+    fs.readFile("message.txt", {encoding:"utf-8"},(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(`data from file>>>` + data);
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        res.write(`<body>${data}</body>`)
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write("</html>");
+        return res.end();
+    })
+  
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+
+    //handler
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    //handler
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        if(err){
+            console.log(err);
+        }
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+});
+
+server.listen(3000);
