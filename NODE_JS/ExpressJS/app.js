4393216b02ab878bcdf51a572a9cc23fad325871
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-06-16 13:04:31 +0530

    add product name and size

--- NODE_JS/ExpressJS/app.js
+++ NODE_JS/ExpressJS/app.js
@@ -1,15 +1,23 @@
-
 const express=require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('in first middleware');
-    next();
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product',(req,res,next)=>{
+    
+    res.send('<html><body><h1>Add product name:</h1><form action="/product" method="POST"><input type="text" name="title"><br/><h1>Add product size:</h1><form action="/product" method="POST"><input type="text" name="size"><br/><button type="submit">Submit</button></form></body></html>')
 });
 
-app.use((req,res,next)=>{
-    console.log('in second one')
-    res.send({key1:"value"})
-})
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+
+app.use('/',(req,res,next)=>{
+    res.send("<h1>Hello to root page</h1>")
+});
+
 app.listen(3000);
