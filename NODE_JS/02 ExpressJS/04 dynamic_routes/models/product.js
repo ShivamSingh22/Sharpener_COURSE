bc1a73159af4f20b02e077091b5450d318db4204
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-07-07 14:11:25 +0530

    dynamic routes 2

--- NODE_JS/02 ExpressJS/04 dynamic_routes/models/product.js
+++ NODE_JS/02 ExpressJS/04 dynamic_routes/models/product.js
@@ -38,6 +38,7 @@ module.exports = class Product {
   static findById(id,cb){
     getProductsFromFile(products => {
       const product=products.find(p => p.id === id);
+      console.log(`Ye product mila h id se ${product}`);
       cb(product);
     });
   }
