e95b19d6f796647c5a2d3ab3da5d4ec14f0625b3
ShivamSingh22 <singh.shivam2238@gmail.com>
2024-07-15 23:46:51 +0530

    added sequelize config

--- NODE_JS/02 ExpressJS/04 dynamic_routes/models/product.js
+++ NODE_JS/02 ExpressJS/04 dynamic_routes/models/product.js
@@ -1,30 +1,27 @@
-const Cart = require('./cart');
-const db = require('../util/database');
+const Sequelize = require('sequelize')
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize = require('../util/database')
+
+const Product = sequelize.define('product',{
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement : true,
+    allowNull : false,
+    primaryKey : true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type : Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl : {
+    type : Sequelize.STRING,
+    allowNull : false
+  },
+  description : {
+    type : Sequelize.STRING,
+    allowNull : false
   }
+})
 
-  save() {
-    return db.execute('INSERT INTO products(title,price,description,imageUrl) VALUES(?,?,?,?)',
-    [this.title,this.price,this.description,this.imageUrl]
-    );
-  }
-
-  static deleteById(id) {
-    return db.execute('DELETE FROM PRODUCTS WHERE PRODUCTS.id = ?',[id]);
-  }
-
-  static fetchAll() {
-   return db.execute('SELECT * FROM PRODUCTS');
-  }
-
-  static findById(id) {
-   return db.execute('SELECT * FROM PRODUCTS WHERE PRODUCTS.id = ?',[id]);
-}
-}
+module.exports = Product;
--- NODE_JS/02 ExpressJS/04 dynamic_routes/util/database.js
+++ NODE_JS/02 ExpressJS/04 dynamic_routes/util/database.js
@@ -1,10 +1,8 @@
-const mysql=require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete','root','Jitendra@123', {
+    dialect: 'mysql',
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Jitendra@123' 
 });
 
-module.exports=pool.promise();
+module.exports = sequelize;
